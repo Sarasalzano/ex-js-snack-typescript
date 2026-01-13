3308ff6fc8ff30d044873e3a2835ec145c5e5c30
Sara Salzano <sarasalzano95@gmail.com>
2026-01-13 11:23:41 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+// Snack 1
+// Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia… Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
+// Se è una stringa: stampala in maiuscolo
+// Se è un numero: moltiplicalo per due e stampalo
+// Se è un booleano: stampa “Sì” o “No” in base al suo valore
+// In tutti gli altri casi: stampa “Tipo non supportato”
+
